9e6bb647a8c99d86bd083a469cedae1c66206b61
juny <king932z@naver.com>
2023-05-16 22:33:26 +0900

    1.2 DAO분리 1.2.1 관심사의 분리  리팩토링, 메소드 추출

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -6,9 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:h2:tcp://localhost/~/springbook", "sa", "");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
@@ -24,9 +22,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:h2:tcp://localhost/~/springbook", "sa", "");
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
@@ -45,5 +41,12 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:h2:tcp://localhost/~/springbook", "sa", "");
+        return c;
     }
 }
