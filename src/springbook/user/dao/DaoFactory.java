83d2900e418db4fca86b9dd9c0195ce79754b0be
juny <king932z@naver.com>
2023-05-28 12:43:51 +0900

    1.4 제어의 역전(Ioc)

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -3,8 +3,16 @@ package springbook.user.dao;
 public class DaoFactory {
 
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
+        return new UserDao(connectionMaker());
+    }
+//    public AccountDao accountDao() {
+//        return new AccountDao(new DConnectionMaker());
+//    }
+//    public MessageDao messageDao() {
+//        return new MessageDao(new DConnectionMaker());
+//    }
+
+    public ConnectionMaker connectionMaker(){
+        return new DConnectionMaker();
     }
 }
